75181f81ae983d53bca942bea523c47972e5ffc1
hodanghuy <hodanghuy977@gmail.com>
2023-12-08 20:27:03 +0700

    set up stripe

---
+++ backend/DoAnMonHoc_Backend/DoAnMonHoc_Backend/Controllers/StripeController.cs
@@ -0,0 +1,40 @@
+﻿using DoAnMonHoc_Backend.Interfaces;
+using DoAnMonHoc_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace DoAnMonHoc_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StripeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StripeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        public async Task<IActionResult> ProcessPayment([FromBody] PaymentRequest paymentRequest)
+        {
+            StripeConfiguration.ApiKey = _configuration["StripeSetting:SecretKey"];
+
+            var options = new ChargeCreateOptions
+            {
+                Amount = paymentRequest.Amount,
+                Currency = "usd",
+                Description = paymentRequest.Description,
+                Source = paymentRequest.Token // Stripe token from client-side
+            };
+
+            var service = new ChargeService();
+            var charge = await service.CreateAsync(options);
+
+            // Xử lý kết quả thanh toán ở đây
+            // ...
+
+            return Ok();
+        }
+    }
+}
